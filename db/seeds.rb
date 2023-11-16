ec321898ba03a8875018b12ce3ba6c7d79748534
felmsena <fm.monsalves@gmail.com>
2023-11-16 00:22:50 -0300

    Change function to a PORO

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,60 +8,6 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-require 'net/http'
-require 'json'
-
-require 'net/http'
-require 'json'
-
-class PokemonDataLoader
-  API_BASE_URL = 'https://pokeapi.co/api/v2/'
-
-  def self.load_pokemon_data
-    limit = 1017
-    pokemon_list_url = "#{API_BASE_URL}pokemon?limit=#{limit}"
-
-    data = fetch_data_from_url(pokemon_list_url)
-    return unless data
-
-    total_pokemon = data['results'].size
-    loaded_count = 0
-
-    data['results'].each do |pokemon|
-      pokemon_data = fetch_data_from_url(pokemon['url'])
-      create_pokemon_from_data(pokemon_data)
-
-      loaded_count += 1
-      display_progress(loaded_count, total_pokemon)
-    end
-  rescue StandardError => e
-    puts "Error occurred: #{e.message}"
-  end
-
-  def self.fetch_data_from_url(url)
-    response = Net::HTTP.get(URI(url))
-    JSON.parse(response) if response.present?
-  end
-
-  def self.create_pokemon_from_data(pokemon_data)
-    return unless pokemon_data
-
-    attributes = {
-      name: pokemon_data['name'],
-      order: pokemon_data['order'],
-      base_experience: pokemon_data['base_experience'],
-      height: pokemon_data['height'],
-      weight: pokemon_data['weight'],
-      types: pokemon_data['types'].map { |type| type['type']['name'] }
-    }
-
-    Pokemon.create(attributes)
-  end
-
-  def self.display_progress(loaded_count, total_pokemon)
-    percent = ((loaded_count.to_f / total_pokemon) * 100).to_i
-    puts "Loading: #{percent}% complete (#{loaded_count}/#{total_pokemon} Pokemon loaded)"
-  end
-end
+require_relative '../lib/loads/pokemon_data_loader'
 
 PokemonDataLoader.load_pokemon_data
