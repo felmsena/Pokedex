68144a8fc9651447a34b3fe406fad32824c416e1
felmsena <fm.monsalves@gmail.com>
2023-11-16 00:23:54 -0300

    Add validation to POST Pokemon
    Add validation if empty, don't create it

--- app/controllers/api/v1/pokemons_controller.rb
+++ app/controllers/api/v1/pokemons_controller.rb
@@ -14,11 +14,15 @@ class Api::V1::PokemonsController < ApplicationController
   end
 
   def create
-    pokemon = Pokemon.new(pokemon_params)
-    if pokemon.save
-      render json: pokemon.as_json, status: :created
+    if create_with_valid_params
+      pokemon = Pokemon.new(pokemon_params)
+      if pokemon.save
+        render json: pokemon.as_json, status: :created
+      else
+        render json: { errors: pokemon.errors.full_messages }, status: :unprocessable_entity
+      end
     else
-      render json: {errors: pokemon.errors.full_messages}, status: :unprocessable_entity
+      render json: { errors: ["Invalid or missing parameters for Pokemon creation"] }, status: :unprocessable_entity
     end
   end
 
@@ -46,5 +50,11 @@ class Api::V1::PokemonsController < ApplicationController
 
   def pokemon_params
     params.require(:pokemon).permit(:name, :order, :base_experience, :height, :weight, types: [])
+  end
+
+  def create_with_valid_params
+    required_params = %i[name order base_experience height weight types]
+
+    required_params.all? { |param| params.dig(:pokemon, param).present? }
   end
 end
